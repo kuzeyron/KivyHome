b303c36fab8f5f512ee3ff4477b994d833c188e7
Mathias Lindström <kuzeyron@gmail.com>
2025-07-17 16:59:25 +0300

    A couple of changes with the app loader

--- libs/icons/platforms/linux.py
+++ libs/icons/platforms/linux.py
@@ -1,11 +1,7 @@
-import time
-from io import BytesIO
-from os import makedirs
-from os.path import getmtime, isfile, join
-from shutil import rmtree
-
-from android.storage import app_storage_path
-from jnius import autoclass, cast
+from glob import glob
+from os import listdir
+from os.path import isfile, join
+from re import split as splitter
 
 from kivy.core.image import Image
 from kivy.logger import Logger
@@ -15,11 +11,10 @@ __all__ = ('GetPackages', )
 class GetPackages:
     apps_path: str = '/usr/share/applications'
 
-    def ready(self):
-        self._home_widget = KivyHome()
+    def find_applications(self):
         applications = sorted([x for x in listdir(self.apps_path) if x.endswith('.desktop')])
         self.amount_of_applications = len(applications)
-        Clock.schedule_once(partial(self.on_busy, True), 0)
+        self.dispatch('on_busy', True)
 
         for step, application in enumerate(applications, 1):
             with open(join(self.apps_path, application), encoding='utf-8') as file:
@@ -38,3 +33,7 @@ class GetPackages:
                         Logger.debug('[KivyHome] Loading icon: %s', icon_path)
                         icon = Image(icon_path)
                         self.add_one(step, name=name, package=application, texture=icon, old=True)
+
+                        break
+
+        self.dispatch('on_busy', False)
